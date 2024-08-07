a9cd40e2231e197cbf3b001938094a398073ae91
Mayank <mayanksingh6787@gmail.com>
2024-08-07 22:03:52 +0530

    Commiting the f

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/* Sidebar */}
+      <Sidebar />
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Mayank singh
+        </motion.span>
+        <div className="social">
+          <a href="https://www.linkedin.com/in/mayank-singh6787/">
+            <img src="/LinkedIn.png" alt="" />
+          </a>
+          <a href="https://github.com/Mayank6787">
+            <img src="/Git.png" alt="" />
+          </a>
+          <a href="https://dribbble.com/Mayank6787">
+            <img src="/dribbble.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
